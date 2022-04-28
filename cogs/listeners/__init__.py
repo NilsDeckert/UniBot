df94517e580f8a27cbf92412b348d47abe0a8ebf
TheHllm <hllmollenkopf@gmail.com>
2022-04-28 22:31:05 +0200

    Fixed minor bugs by removing part(s) of the code

--- cogs/listeners/__init__.py
+++
@@ -1,7 +0,0 @@
-from discord.ext.commands import Bot
-
-from .event_listeners import Listen
-
-
-def setup(bot: Bot):
-    bot.add_cog(Listen(bot))
--- cogs/user/__init__.py
+++
@@ -1,8 +0,0 @@
-from main import UniBot
-from .tu_specific import TUB
-from .user_commands import User
-
-
-def setup(bot: UniBot):
-    bot.add_cog(User(bot))
-    bot.add_cog(TUB(bot))
